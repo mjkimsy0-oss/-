8628524fe310a2a288735b0e6a1044266a21661d
mjkimsy0-oss <mjkimsy0@gmail.com>
2026-07-26 21:07:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "link-archive-v19";
+const CACHE_NAME = "link-archive-v20";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(event){
